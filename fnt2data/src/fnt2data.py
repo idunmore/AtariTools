46191d2ee32c8371236ad541e6933d9993a29e58
idunmore <62761638+idunmore@users.noreply.github.com>
2024-05-09 11:04:20 -0400

    Initial commit

---
+++ fnt2data/src/fnt2data.py
@@ -0,0 +1,10 @@
+#!python3
+
+# fn2data.py - Font to Data Converter
+#
+# Copyright(C) 2024, Ian Michael Dunmore
+#
+# License: https://github.com/idunmore/AtariTools/blob/master/LICENSE
+
+# 3rd Party/External Modules
+import click
